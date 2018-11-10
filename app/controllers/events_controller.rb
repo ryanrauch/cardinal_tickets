737ef2c34d8c8d2fa9128d4771523ba4bca9a8e6
ryanrauch <ryan.rauch@outlook.com>
2018-11-10 00:47:44 -0600

    user must exist error fixed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,6 +25,7 @@ class EventsController < ApplicationController
   # POST /events.json
   def create
     @event = Event.new(event_params)
+    #@event = current_user.events.create(event_params)
 
     respond_to do |format|
       if @event.save
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,4 @@
 class Ticket < ApplicationRecord
   belongs_to :event
-  belongs_to :user
+  belongs_to :user, optional: true
 end
